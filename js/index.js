e546924412b4d71c2170239ce9565fbf4c8cc718
heptaluan <vd2o@foxmail.com>
2022-08-29 14:28:58 +0800

    add

--- js/index.js
+++ js/index.js
@@ -12,27 +12,6 @@ myChart.showLoading()
 echarts.registerMap('xinxiang', geoData)
 myChart.hideLoading()
 
-let geoCoordMap = {
-  海门: [121.15, 31.89],
-  鄂尔多斯: [109.781327, 39.608266],
-  招远: [120.38, 37.35],
-}
-
-var convertData = function (data) {
-  var res = []
-  for (var i = 0; i < data.length; i++) {
-    var geoCoord = geoCoordMap[data[i].name]
-    if (geoCoord) {
-      res.push({
-        name: data[i].name,
-        value: geoCoord.concat(data[i].value),
-      })
-    }
-  }
-  // console.log(res)
-  return res
-}
-
 var option = {
   title: {
     text: '测试bar3D、scatter3D、geo3D',
@@ -50,29 +29,6 @@ var option = {
     //   return data;
     // },
   },
-  visualMap: [
-    {
-      type: 'continuous',
-      seriesIndex: 0,
-      text: ['bar3D'],
-      calculable: true,
-      max: 300,
-      inRange: {
-        color: ['#87aa66', '#eba438', '#d94d4c'],
-      },
-    },
-    {
-      type: 'continuous',
-      seriesIndex: 1,
-      text: ['scatter3D'],
-      left: 'right',
-      max: 100,
-      calculable: true,
-      inRange: {
-        color: ['#000', 'blue', 'purple'],
-      },
-    },
-  ],
   geo3D: {
     map: 'xinxiang',
     roam: true,
@@ -91,7 +47,7 @@ var option = {
       show: true,
       textStyle: {
         color: '#fff', //地图初始化区域字体颜色
-        fontSize: 16,
+        fontSize: 14,
         opacity: 1,
         backgroundColor: 'rgba(0,23,11,0)',
       },
@@ -102,7 +58,7 @@ var option = {
         show: true,
         textStyle: {
           color: '#fff',
-          fontSize: 16,
+          fontSize: 14,
           backgroundColor: 'rgba(0,23,11,0)',
         },
       },
@@ -123,79 +79,7 @@ var option = {
       },
     },
   },
-  series: [
-    //柱状图
-    {
-      name: 'bar3D',
-      type: 'bar3D',
-      coordinateSystem: 'geo3D',
-      barSize: 1, //柱子粗细
-      shading: 'lambert',
-      opacity: 1,
-      bevelSize: 0.3,
-      label: {
-        show: false,
-        formatter: '{b}',
-      },
-      data: convertData([
-        {
-          name: '海门',
-          value: (Math.random() * 300).toFixed(2),
-        },
-        {
-          name: '鄂尔多斯',
-          value: (Math.random() * 300).toFixed(2),
-        },
-        {
-          name: '招远',
-          value: (Math.random() * 300).toFixed(2),
-        },
-      ]),
-    },
-
-    {
-      name: 'scatter3D',
-      type: 'scatter3D',
-      coordinateSystem: 'geo3D',
-      symbol: 'pin',
-      symbolSize: 30,
-      opacity: 1,
-      label: {
-        show: false,
-        formatter: '{b}',
-      },
-      itemStyle: {
-        borderWidth: 0.5,
-        borderColor: '#fff',
-      },
-      data: convertData([
-        {
-          name: '阳泉',
-          value: (Math.random() * 100 + 50).toFixed(2),
-        },
-        {
-          name: '莱州',
-          value: (Math.random() * 100 + 50).toFixed(2),
-        },
-        {
-          name: '湖州',
-          value: (Math.random() * 100 + 50).toFixed(2),
-        },
-        {
-          name: '汕头',
-          value: (Math.random() * 100 + 50).toFixed(2),
-        },
-        {
-          name: '昆山',
-          value: (Math.random() * 100 + 50).toFixed(2),
-        },
-        {
-          name: '张家口',
-          value: (Math.random() * 100 + 50).toFixed(2),
-        },
-      ]),
-    },
-  ],
+  series: [],
 }
 
 myChart.setOption(option)
