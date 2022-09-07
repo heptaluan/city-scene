194fc8896a6bdb8a64b9cd944888b326df996b98
heptaluan <vd2o@foxmail.com>
2022-09-07 16:28:59 +0800

    add

--- js/options.js
+++ js/options.js
@@ -7,242 +7,137 @@ export const echarts_1 = () => {
   // 基于准备好的dom，初始化echarts实例
   var myChart = echarts.init(document.getElementById('echart1'))
 
-  let bgColor = '#040811'
-  let color = ['#0090FF', '#EE8931', '#8B5CFF']
-  let echartData = [
-    {
-      name: '1月',
-      value1: 13512,
-      value2: 0,
-      value3: 0,
-    },
-    {
-      name: '2月',
-      value1: 9806,
-      value2: 0,
-      value3: 0,
-    },
-    {
-      name: '3月',
-      value1: 19727,
-      value2: 0,
-      value3: 0,
-    },
-    {
-      name: '4月',
-      value1: 17046,
-      value2: 12689,
-      value3: 7479,
-    },
-    {
-      name: '5月',
-      value1: 17780,
-      value2: 13396,
-      value3: 6355,
-    },
-    {
-      name: '6月',
-      value1: 19359,
-      value2: 13220,
-      value3: 5940,
-    },
-    {
-      name: '7月',
-      value1: 16229,
-      value2: 11836,
-      value3: 10234,
-    },
-    {
-      name: '8月',
-      value1: 18176,
-      value2: 13478,
-      value3: 10755,
-    },
-    {
-      name: '9月',
-      value1: 18515,
-      value2: 12911,
-      value3: 9806,
-    },
-    {
-      name: '10月',
-      value1: 13658,
-      value2: 9479,
-      value3: 8613,
-    },
-    {
-      name: '11月',
-      value1: 16364,
-      value2: 10839,
-      value3: 8826,
-    },
-    {
-      name: '12月',
-      value1: 13973,
-      value2: 8816,
-      value3: 7291,
-    },
-  ]
-
-  let legendItem = ['实际接受服务', '持续接受服务', '完成两次面对面服务']
-
-  let xAxisData = echartData.map(v => v.name)
-  //  ["1", "2", "3", "4", "5", "6", "7", "8"]
-  let yAxisData1 = echartData.map(v => v.value1)
-  // [100, 138, 350, 173, 180, 150, 180, 230]
-  let yAxisData2 = echartData.map(v => v.value2)
-  // [233, 233, 200, 180, 199, 233, 210, 180]
-  let yAxisData3 = echartData.map(v => v.value3)
-  //[133,133,100,80,99,133,110,80]
-  const hexToRgba = (hex, opacity) => {
-    let rgbaColor = ''
-    let reg = /^#[\da-f]{6}$/i
-    if (reg.test(hex)) {
-      rgbaColor = `rgba(${parseInt('0x' + hex.slice(1, 3))},${parseInt('0x' + hex.slice(3, 5))},${parseInt(
-        '0x' + hex.slice(5, 7)
-      )},${opacity})`
-    }
-    return rgbaColor
-  }
-
   var option = {
-    color: color,
-    legend: {
-      show: false,
-    },
     tooltip: {
       trigger: 'axis',
-      formatter: function (params) {
-        let html = ''
-        params.forEach(v => {
-          console.log(v)
-          html += `<div style="color: #666;font-size: 14px;line-height: 24px">
-                <span style="display:inline-block;margin-right:5px;border-radius:10px;width:10px;height:10px;background-color:${
-                  color[v.componentIndex]
-                };"></span>
-                ${v.seriesName}.${v.name}
-                <span style="color:${color[v.componentIndex]};font-weight:700;font-size: 18px">${v.value}</span>
-                万元`
-        })
-        return html
+      axisPointer: {
+        lineStyle: {
+          color: '#dddc6b',
+        },
       },
-      extraCssText: 'background: #fff; border-radius: 0;box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);color: #333;',
     },
     xAxis: [
       {
         type: 'category',
-        axisTick: {
-          show: false,
+        boundaryGap: false,
+        axisLabel: {
+          textStyle: {
+            color: 'rgba(255,255,255,.6)',
+            fontSize: 12,
+          },
         },
         axisLine: {
-          show: true,
           lineStyle: {
-            type: 'dashed',
-            color: 'rgba(255, 255, 255,0.5)',
+            color: 'rgba(255,255,255,.2)',
           },
         },
-        axisLabel: {
-          inside: false,
-          textStyle: {
-            color: 'rgba(255, 255, 255,0.7)',
-            fontWeight: 'normal',
-            fontSize: '14',
-            lineHeight: 22,
-          },
-        },
-
-        data: xAxisData,
+        data: [
+          '01',
+          '02',
+          '03',
+          '04',
+          '05',
+          '06',
+          '07',
+          '08',
+          '09',
+          '11',
+          '12',
+          '13',
+          '14',
+          '15',
+          '16',
+          '17',
+          '18',
+          '19',
+          '20',
+          '21',
+          '22',
+          '23',
+          '24',
+        ],
+      },
+      {
+        axisPointer: { show: false },
+        axisLine: { show: false },
+        position: 'bottom',
+        offset: 20,
       },
     ],
     yAxis: [
       {
         type: 'value',
-        // name: '(t)',
+        axisTick: { show: false },
+        axisLine: {
+          lineStyle: {
+            color: 'rgba(255,255,255,.1)',
+          },
+        },
         axisLabel: {
           textStyle: {
-            color: 'rgba(255, 255, 255,0.7)',
+            color: 'rgba(255,255,255,.6)',
+            fontSize: 12,
           },
         },
-        nameTextStyle: {
-          color: 'rgba(255, 255, 255,0.7)',
-          fontSize: 12,
-          lineHeight: 40,
-          padding: [0, 0, 0, -20],
-        },
+
         splitLine: {
-          show: false,
           lineStyle: {
-            type: 'dashed',
-            color: 'rgba(255, 255, 255,0.5)',
+            color: 'rgba(255,255,255,.1)',
           },
-        },
-        axisLine: {
-          show: true,
-          lineStyle: {
-            type: 'dashed',
-            color: 'rgba(255, 255, 255,0.5)',
-          },
-        },
-        axisTick: {
-          show: false,
         },
       },
     ],
     series: [
       {
-        name: legendItem[0],
+        name: '安卓',
         type: 'line',
-        showSymbol: false,
         smooth: true,
-        symbolSize: 8,
-        zlevel: 3,
+        symbol: 'circle',
+        symbolSize: 5,
+        showSymbol: false,
         lineStyle: {
           normal: {
-            color: color[0],
-            shadowBlur: 3,
-            shadowColor: hexToRgba(color[0], 0.5),
-            shadowOffsetY: 0,
+            color: '#0184d5',
+            width: 2,
           },
         },
-        data: yAxisData1,
-      },
-      {
-        name: legendItem[1],
-        type: 'line',
-        showSymbol: false,
-        smooth: true,
-        symbolSize: 8,
-        zlevel: 3,
-        lineStyle: {
+        areaStyle: {
           normal: {
-            color: color[1],
-            shadowBlur: 0,
-            shadowColor: hexToRgba(color[1], 0.5),
-            shadowOffsetY: 0,
+            color: new echarts.graphic.LinearGradient(
+              0,
+              0,
+              0,
+              1,
+              [
+                {
+                  offset: 0,
+                  color: 'rgba(1, 132, 213, 0.4)',
+                },
+                {
+                  offset: 0.8,
+                  color: 'rgba(1, 132, 213, 0.1)',
+                },
+              ],
+              false
+            ),
+            shadowColor: 'rgba(0, 0, 0, 0.1)',
           },
         },
-        data: yAxisData2,
-      },
-      {
-        name: legendItem[2],
-        type: 'line',
-        showSymbol: false,
-        smooth: true,
-        symbolSize: 8,
-        zlevel: 3,
-        lineStyle: {
+        itemStyle: {
           normal: {
-            color: color[2],
-            shadowBlur: 3,
-            shadowColor: hexToRgba(color[2], 0.5),
-            shadowOffsetY: 0,
+            color: '#0184d5',
+            borderColor: 'rgba(221, 220, 107, .1)',
+            borderWidth: 12,
           },
         },
-        data: yAxisData3,
+        data: [3, 4, 3, 4, 3, 4, 3, 6, 2, 4, 2, 4, 3, 4, 3, 4, 3, 4, 3, 6, 2, 4, 2, 4],
       },
+      
     ],
+    animationDelay: animationDelay,
   }
 
+  // 使用刚指定的配置项和数据显示图表。
   myChart.setOption(option)
   window.addEventListener('resize', function () {
     myChart.resize()
@@ -581,57 +476,57 @@ export const echarts_4 = () => {
   var myChart = echarts.init(document.getElementById('echart4'))
 
   var option = {
+    //  backgroundColor: '#00265f',
     tooltip: {
       trigger: 'axis',
       axisPointer: {
         type: 'shadow',
       },
     },
+    grid: {
+      left: '0%',
+      top: '10px',
+      right: '0%',
+      bottom: '4%',
+      containLabel: true,
+    },
     xAxis: [
       {
         type: 'category',
-        data: [
-          'SQL注入',
-          '非法访问',
-          '其他',
-          'DOS攻击',
-          '信息爬虫',
-          '进出',
-          '业务主机',
-          '扫描器',
-          '恶意攻击',
-          '攻击协助',
-        ],
+        data: ['商超门店', '教育培训', '房地产', '生活服务', '汽车销售', '旅游酒店', '五金建材'],
         axisLine: {
+          show: true,
           lineStyle: {
-            color: '#FFFFFF',
+            color: 'rgba(255,255,255,.1)',
+            width: 1,
+            type: 'solid',
           },
+        },
+
+        axisTick: {
+          show: false,
         },
         axisLabel: {
           interval: 0,
-        },
-        axisTick: {
-          show: false,
+          // rotate:50,
+          show: true,
+          splitNumber: 15,
+          textStyle: {
+            color: 'rgba(255,255,255,.6)',
+            fontSize: '12',
+          },
         },
       },
     ],
     yAxis: [
       {
-        min: 0,
-        max: 4000,
-        interval: 1000,
         type: 'value',
         axisLabel: {
+          //formatter: '{value} %'
+          show: true,
           textStyle: {
-            color: '#ffffff',
-          },
-          formatter: function (value, index) {
-            if (value >= 1000) {
-              value = value / 1000 + '000'
-            } else if (value < 1000) {
-              value
-            }
-            return value
+            color: 'rgba(255,255,255,.6)',
+            fontSize: '12',
           },
         },
         axisTick: {
@@ -640,12 +535,14 @@ export const echarts_4 = () => {
         axisLine: {
           show: true,
           lineStyle: {
-            color: '#FFFFFF',
+            color: 'rgba(255,255,255,.1	)',
+            width: 1,
+            type: 'solid',
           },
         },
         splitLine: {
           lineStyle: {
-            color: 'rgba(255,255,255,0.12)',
+            color: 'rgba(255,255,255,.1)',
           },
         },
       },
@@ -653,12 +550,18 @@ export const echarts_4 = () => {
     series: [
       {
         type: 'bar',
-        data: [2000, 1900, 1700, 1400, 1000, 800, 600, 400, 200, 100],
-        barWidth: '22px',
+        data: [200, 300, 300, 900, 1500, 1200, 600],
+        barWidth: '35%', //柱子宽度
+        // barGap: 1, //柱子之间间距
         itemStyle: {
           normal: {
-            color: 'rgb(24,176,255)',
+            color: '#2f89cf',
+            opacity: 1,
+            barBorderRadius: 5,
           },
+        },
+        animationDelay: function (idx) {
+          return idx * animationDelay
         },
       },
     ],
